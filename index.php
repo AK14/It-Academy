28ba7c9358688bc472594e5bb42aa122daff799f
ak_14 <hooploop14@gmail.com>
2017-02-21 21:08:56 +0300

    скачал bootstrap новая страница forum
    Через composer установил bootstrap
    создал: resource/view (поддериктория для view-х)
    создал: forum.php во view - форма добавления нового сообщения.

--- index.php
+++ index.php
@@ -5,8 +5,12 @@ include "classes/files.php";
 
 //use Classes\ksr1;
 use Classes\files;
+echo '<div class="jumbotron">';
+echo " перейти на <a href='./resource/view/forum.php'> Форум </a><br>";
+echo '</div>';
 
-echo "<form method='post' action='index.php' enctype='multipart/form-data'>
+
+/*echo "<form method='post' action='index.php' enctype='multipart/form-data'>
     <head>
     Форма регистрации<br>
     </head>
@@ -20,6 +24,9 @@ echo "<form method='post' action='index.php' enctype='multipart/form-data'>
 if(isset($_FILES) & $_FILES['file']['error'] == 0){
  move_uploaded_file($_FILES['file']['tmp_name'],'files/'.$_FILES['file']['name']);
 }
+*/
+
+
 
 // Создаем объект класса files
 $search = new files("files/zend.txt");
---
+++ resource/view/forum.php
@@ -0,0 +1,44 @@
+<?php
+// если было отправка методом POST
+    if(isset($_POST) ){
+       $name = $_POST['author'];
+       $text = $_POST['text'];
+       echo $name."<br>" . $text."<br>";
+    }
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>
+        Hello friend.
+    </title>
+
+    <!-- Styles -->
+    <link href="../../vendor/twbs/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script type="text/javascript" href="../../vendor/twbs/bootstrap/dist/js/bootstrap.js">
+
+    </script>
+</head>
+<body>
+
+
+<section class="container">
+<!--    форма добавления нового комментария -->
+    <form class="form-horizontal col-md-6 col-md-offset-3" method="post" action="forum.php">
+        <label for="author"> Автор </label>
+        <input type="text" class="form-control" name="author">
+        <label for="text"> Сообщение </label>
+        <textarea class="form-control" name="text"></textarea>
+        <br>
+        <input type="submit" class="btn bg-info" value="submit">
+    </form>
+</section>
+
+</body>
+</html>
+
